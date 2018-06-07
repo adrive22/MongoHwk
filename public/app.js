3ca69dce64238d9a548b1c7ad8a44bde8f3dd6bc
Allison Driver <allisondriver@allisons-mbp.lan>
2018-06-07 14:48:28 -0500

    added article comments buttons

--- public/app.js
+++ public/app.js
@@ -1,12 +1,22 @@
+var empty= function(){
+$(".articlesHeadlines").empty();
+}
+empty();
+
 
 //Grab the articles as a json   
 $.getJSON("/all", function(data) {
 
-    //for each one
-    for (var i = 0; i < data.length; i++) {
+   // for each one
+    for (var i = 0; i < 25; i++) {
 
-        //Display the info on the html
-        $(".articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title +"<br />" + data[i].link + "</p");
+//        // Display the info on the html
+//         $(".articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title +"<br />" + data[i].link + "</p");
+//    }
+
+
+   $(".articlesHeadlines").append("<p>" + data[i].title + "</p>" + "<button type='button'" + "class='btn btn-primary testBtn'" + "data-toggle='modal'" + "data-target='#exampleModal'"+"</button>");
+   $(".testBtn").text("Article Comments");
+   
     }
-
- });
+});
